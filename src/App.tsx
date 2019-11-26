c333d6e07940a31668611ae2f92abe9182f63a86
Claudio Ricci <claudioricci@Claudios-MBP.home>
2019-11-26 12:36:30 +0000

    add useParams Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import MainContainer from "./components/MainContainer";
+import "./App.scss";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Panel from "./components/Panel";
+// import NavBar from "./components/NavBar";
+// import MainContainer from "./components/MainContainer";
 
-const App = () => {
+export default function App() {
   return (
-    <>
-      <NavBar />
-      <MainContainer />
-    </>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            <li>
+              <Link to="/page/page1">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page/page2">Page 2</Link>
+            </li>
+            <li>
+              <Link to="/page/page3">Page 3</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/page/:page" component={Panel} />
+            {/* <Route exact path="/page2" component={Panel} />
+            <Route exact path="/page3" component={Panel} /> */}
+          </Switch>
+        </header>
+      </div>
+    </Router>
   );
-};
-
-export default App;
+}
--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import "./Panel.scss";
 import uuid from "uuid";
 import { pure } from "recompose";
-import { PanelProps } from "../../interfaces/";
+import { useParams } from "react-router";
+// import { PanelProps } from "../../interfaces/";
 
-const Panel: React.SFC<PanelProps> = props => {
+function Panel() {
+  const { page } = useParams();
   return (
     <main key={uuid.v4()} role="main" data-testid="Panel">
-      <h1>{props.title}</h1>
-      <p>{props.description}</p>
-
-      <p>Test</p>
+      <p>Page: {page}</p>
     </main>
   );
-};
+}
 
 export default pure(Panel);
